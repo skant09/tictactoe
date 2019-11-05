875fb59edb984a83f15849ada03529e57d309db2
skant09 <suryakant.iitm@gmail.com>
2019-11-05 07:48:17 +0530

    Updates

---
+++ precache-manifest.5de893a61aa4cdd172ab2cf520acbb02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ba72a1c61566da4b2da811c5d00ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2deac82497bc8ebbf28",
+    "url": "/static/css/main.d6135423.chunk.css"
+  },
+  {
+    "revision": "09b56845eca725566619",
+    "url": "/static/js/2.dd099561.chunk.js"
+  },
+  {
+    "revision": "c2deac82497bc8ebbf28",
+    "url": "/static/js/main.a52d2992.chunk.js"
+  },
+  {
+    "revision": "63281be984038bd27edc",
+    "url": "/static/js/runtime-main.fb500502.js"
+  }
+]);
