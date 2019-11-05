4bc85751404c81b51d96ad9076015fb82c9c87fe
skant09 <suryakant.iitm@gmail.com>
2019-11-05 08:33:43 +0530

    Updates

---
+++ precache-manifest.2593840586ef416fc448330ce56b97e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63e31db96eca90dfe6693cdbeac965d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb9d667c5d13c909611f",
+    "url": "/static/css/main.d6135423.chunk.css"
+  },
+  {
+    "revision": "09b56845eca725566619",
+    "url": "/static/js/2.dd099561.chunk.js"
+  },
+  {
+    "revision": "eb9d667c5d13c909611f",
+    "url": "/static/js/main.ada4c107.chunk.js"
+  },
+  {
+    "revision": "63281be984038bd27edc",
+    "url": "/static/js/runtime-main.fb500502.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tictactoe/precache-manifest.b6d92b05af2e44cca3162604efd5dbd7.js"
+  "/precache-manifest.2593840586ef416fc448330ce56b97e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tictactoe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
