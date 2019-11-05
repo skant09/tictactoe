b25be6154a2f629cae9c5d6f69f008c54c163874
skant09 <suryakant.iitm@gmail.com>
2019-11-05 08:25:08 +0530

    Updates

---
+++ build/precache-manifest.b6d92b05af2e44cca3162604efd5dbd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d0a481b303119d998c101ef1e29099",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "ef81e2beb158e59a2302",
+    "url": "/tictactoe/static/css/main.d6135423.chunk.css"
+  },
+  {
+    "revision": "09b56845eca725566619",
+    "url": "/tictactoe/static/js/2.dd099561.chunk.js"
+  },
+  {
+    "revision": "ef81e2beb158e59a2302",
+    "url": "/tictactoe/static/js/main.95cfe251.chunk.js"
+  },
+  {
+    "revision": "395a9a79bef4d3c5947f",
+    "url": "/tictactoe/static/js/runtime-main.9034d510.js"
+  }
+]);
